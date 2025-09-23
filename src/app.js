6c8a4cfe4006ff1e4c5fac2a18d4566436e87912
Avishek <avishekmukherje007@gmail.com>
2025-09-24 01:54:30 +0530

    Implement ESLint & Prettier

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 // setting up express server
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/',(req, res) => {
-    res.status(200).send("Hello CI!")
-})
+app.get('/', (req, res) => {
+  res.status(200).send('Hello CI!');
+});
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 // runnning server
-import express from 'express'
+import express from 'express';
 
-import app from './app.js'
+import app from './app.js';
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log(`Running Script on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Running Script on http://localhost:${PORT}`);
+});
